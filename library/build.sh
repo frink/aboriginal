e615e04da666342f4df02a4a9c237d68fe125a8e
R. Giles <rgiles@monigle.com>
2013-12-19 23:51:55 -0700

    major updates now has build ready to script

--- library/build.sh
+++ library/build.sh
@@ -4,16 +4,16 @@
 #@function build_if_needed [package]
 #@ - build a package if needed
 build_if_needed() {
+  build_tools_path
+
   # If we need to build cross compiler, assume root filesystem is stale.
-  if [ "$DO_REBUILD" == "$1" ]; then
+  if [ -z "$NO_REBUILD" ]; then
     remove_built "$1"
-  elif [ -f "$DIR_MAKE/$1-$DO_TARGET" ]; then
+  elif [ -f "$DIR_MAKE/$1-$TARGET" ]; then
     return 1
   fi
 
-  remove_built $1
-
-  time $DIR_STGS/$1 || exit_build $1
+  # do build stuff
 
   return 0
 }
@@ -32,41 +32,88 @@ build_stage() {
     source_limit $STAGE_FILE
   done
 }
- 
+
 #@function remove_built [package]
 #@ - remove built packages
 remove_built() {
-  for $stage in "$@"; do
-    rm -f "$DIR_MAKE/$stage-$DO_TARGET.tar.bz2"
+  for package in "$@"; do
+    PATH="$SAVEPATH" rm -f "$DIR_MAKE/$package-$TARGET.tar.bz2"
   done
 }
 
-#@function exit_build [package]
-#@ - exit build to alert a faliure
-exit_build() {
-  echo "Building $1 failed!"
+#@function build_package [build_type]
+#@ - build a given package
+build_package() {
+  local build_type="$1"
 
-  exit 500
+  [ -z "$build_type" ] && show_error "Build type is not specified for build_package"
+
+  in_array $build_type ${BUILD_TYPES[@]} || return 0
+  env_prefix $build_type ${BUILD_ENV[@]}
+
+  [ -z "$FORK" ] && echo -ne "  \E[33mBuilding $build_type version of $CONFIG\E[0m\n"
+
+  build_if_needed $TARGET $CONFIG
 }
 
-#@function build_config [function] [config]
-#@ - build config with stage
-build_config() {
-  local FUNCTION="$1"
-  local CONFIG="$2"
-  export PACKAGES=()
-  export URL=""
-  export SHA1=""
+#@function build_tools_path
+#@ - create path for host tools
+build_tools_path() {
+  build_tools_links
 
-  [ -z "$CONFIG" ] && CONFIG="build"
+  export PATH="$DIR_HOST"
+  local i=1
 
-  source_quiet "$DIR_CONF/$CONFIG.conf"
-  source_quiet "$DIR_CONF/$TARGET/$CONFIG.conf"
-  worker_fork $FUNCTION $CONFIG
+  while [ -e "$DIR_HOST/$i" ]; do
+    PATH="$PATH:$DIR_HOST/$i"
+    i=$[$i+1]
+  done
+}
 
-  while read x; do
-    [ -n "$x" ] && build_config $1 "$x"
-  done < <(echo ${PACKAGES[@]} | sed -e 's/\s/\n/g')
+#@function build_tools_path
+#@ - create symlinks for all versions of build tools
+build_tools_links() {
+  (PATH="$SAVEPATH"
+    rm -rf "$DIR_HOST"
 
-  return 0
+    local i=0
+    local x=0
+    local dir=""
+
+    # Create symlinks to the host toolchain.  We need a usable existing host
+    # toolchain in order to build anything else (even a new host toolchain),
+    # and we don't really want to have to care what the host type is, so
+    # just use the toolchain that's already there.
+
+    # This is a little more complicated than it needs to be, because the host
+    # toolchain may be using ccache and/or distcc, which means we need every
+    # instance of these tools that occurs in the $PATH, in order, each in its
+    # own fallback directory.
+
+    for x in ar as nm cc make ld gcc $HOSTTOOL_EXTRAS; do
+      i=0
+      dir="$DIR_HOST"
+
+      # Loop through each instance, populating fallback directories.
+      which -a "$x" | while read xx; do
+        if [ ! -e "$dir/$x" ]; then
+          mkdir -p "$dir"
+          ln -sfn "$xx" "$dir/$x" || oops
+        fi
+
+        i=$[$i+1]
+        dir="$DIR_HOST/$i"
+      done
+
+      if [ ! -f "$DIR_HOST/$x" ]; then
+        echo -e "  \e[31mToolchain component missing: $x\e[0m\n"
+      fi
+    done
+
+    # Workaround for systems that make gcc a perl script that calls
+    # gcc.real. Do outside of above loop to avoid thowing error
+
+    local gcc_real="$(PATH="$SAVEPATH" "which" gcc.real)"
+    [ ! -z "$gcc_real" ] && ln -s "$gcc_real" "$DIR_HOST/gcc.real" 2>/dev/null
+  )
 }
--- library/utils.sh
+++ library/utils.sh
@@ -9,3 +9,44 @@ blank_dir() {
   [ -z "$NO_CLEANUP" ] && rm -rf "$1"
   mkdir -p "$1" || show_error "Could not create $1"
 }
+
+#@function execute_config [function] [config] [params]
+#@ - execute function with specified config
+execute_config() {
+  local CONFIG="$1"
+  local FUNCTION="${@:2}"
+  export PACKAGES=()
+  export BUILD_TYPES=()
+  export URL=""
+  export SHA1=""
+
+  source_quiet "$DIR_CONF/$CONFIG.conf"
+  source_quiet "$DIR_CONF/$TARGET/$CONFIG.conf"
+  worker_fork $FUNCTION
+
+  for CONFIG in "${PACKAGES[@]}"; do
+    execute_config $CONFIG $FUNCTION
+  done
+
+  return 0
+}
+
+#@function in_array [needle] [haystack]
+#@ - check if an array contains a certain item
+in_array() {
+  local i
+
+  for i in "${@:2}"; do
+    [[ "$i" == "$1" ]] && return 0
+  done
+
+  return 1
+}
+#@function env_prefix [prefix] [variables]
+#@ - setup environment variables from prefix
+env_prefix() {
+  for i in ${@:2}; do
+    local _i="$i_$1"
+    [ -n "${!_i}" ] && export $i=${!_i}
+  done
+}
